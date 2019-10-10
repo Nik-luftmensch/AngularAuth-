6324dc4dd2e1f08f71ba037d7963da9ca4f4b593
Nikhil Singh <nikhil.singh@nvent.com>
2019-10-10 18:30:53 +0530

    Refactoring components

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -10,7 +10,6 @@ export class Auth {
   lock = new Auth0Lock('ZU6zzfUe3ThCC6v8UQaEM69HWzYA1gk6', 'techguyweb.auth0.com', {});
 
   constructor() {
-    // Add callback for lock `authenticated` event
     this.lock.on("authenticated", (authResult:any) => {
       this.lock.getProfile(authResult.idToken, function(error:any, profile:any){
           if(error){
@@ -23,18 +22,16 @@ export class Auth {
   }
 
   public login() {
-    // Call the show method to display the widget.
+
     this.lock.show();
   };
 
   public authenticated() {
-    // Check if there's an unexpired JWT
-    // This searches for an item in localStorage with key == 'id_token'
+ 
     return tokenNotExpired();
   };
 
   public logout() {
-    // Remove token from localStorage
     localStorage.removeItem('id_token');
     localStorage.removeItem('profile');
   };
